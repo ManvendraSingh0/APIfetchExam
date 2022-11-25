49f49675d305eb452ff91872a6ba141a697e8355
Manvendra singh <manvendra.singh_cs20@gla.ac.in>
2022-11-25 23:02:31 +0530

    commit

--- first.js
+++ first.js
@@ -1,34 +1,64 @@
-var search = document.getElementById('sear');
-var form = document.getElementById('form');
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  search_movie();
-});
-function search_movie() {
-    var url_2 = `https://api.tvmaze.com/search/shows?q=${search.value}`;
-    // console.log(search.value);
-    fetch(url_2)
-        .then((res) => {
-            // console.log(res.json());
-            return res.json();
-        })
-        .then((data) => {
-            console.log(data);
-            add_search(data);
+var a = document.getElementById("searchi");
+const button = document.querySelector(".btn");
+const form=document.querySelector("#addform");
+button.addEventListener("click",showimage());   
+function showimage(e) {
+    
+    let aa = a.value;
+    console.log(aa);
+    fetch('https://api.tvmaze.com/search/shows?q=Kung Fu Panda').then(response => {
+        return response.json();
+    }).then(data => {
+        let html = "";
+        console.log(data);
+        data.forEach(d => {
+           
+            let url=d.show.image.original;
+            console.log(url);
+            let h = `<img src=${url}>`;
+            html += h;
+        });
+        document.querySelector(".imager").innerHTML = html;
+    })
+        .catch(error => {
+            console.log(error);
         });
 }
-function add_search(data) {
-      var search_movies = document.querySelector(".s-box"); 
-      var movies = document.querySelector(".moviess");
-      movies.innerHTML='';
-      console.log(data);
-      for (const movie in data) {
-          const imgg = data[movie].show.image.original;
-          // console.log(imgg);
-          movies.innerHTML += `<div class='movie'>
-                      <img src=${imgg} height="200"/>  
-                  </div>`;
-      };
-      search_movies.style.visibility = "visible";
+
+const gray = document.querySelector('.circle1');
+const pink = document.querySelector('.circle2');
+const blue = document.querySelector('.circle3');
+const green = document.querySelector('.circle4');
+const black = document.querySelector('.circle5');
+
+gray.addEventListener('click', () => changeTheme('gray'));
+pink.addEventListener('click', () => changeTheme('pink'));
+blue.addEventListener('click', () => changeTheme('blue'));
+green.addEventListener('click', () => changeTheme('green'));
+black.addEventListener('click', () => changeTheme('black'));
+
+function changeTheme(color) {
+    document.body.style.backgroundColor = color;
     
-  };
+    if (color = "gray") {
+        document.querySelector(".color1").style.backgroundColor = color;
+    }
+    if (color = "green") {
+        document.querySelector(".color2").style.backgroundColor = color;
+    }
+    if (color = "pink") {
+        
+        document.querySelector(".color3").style.backgroundColor = color;
+    }
+    if (color = "blue") {
+        
+        document.querySelector(".color4").style.backgroundColor = color;
+    }
+    if (color = "black") {
+        
+
+        document.querySelector(".color5").style.backgroundColor = color;
+        
+    }
+
+}
